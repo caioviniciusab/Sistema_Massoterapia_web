f475e612b2a8d7baa60f1cc5baa56f14674b5b7d
Caio Vinicius <bezerracaio615@gmail.com>
2026-08-18 18:28:19 -0300

    Testando deployment

--- src/main/java/com/massoterapia/sistemaweb/service/WhatsAppService.java
+++ src/main/java/com/massoterapia/sistemaweb/service/WhatsAppService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.nio.charset.StandardCharsets;
+import java.nio.charset.StandardCharsets; 
 
 @Service
 public class WhatsAppService {
